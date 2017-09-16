2c29ab2111f96ba9d7c5df7997f204f2995c8571
liuhaoxuan <liuhaoxuan@star-net.cn>
2017-09-16 21:54:58 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <m-footer></m-footer>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from './components/m-header/m-header'
+  import MFooter from 'components/m-footer/m-footer'
+
+  export default {
+    components: {
+      MHeader,
+      MFooter
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
