04c00d39aef7dcf799939c29d09862e24828e490
liuhaoxuan <liuhaoxuan@star-net.cn>
2017-09-18 23:12:08 +0800

    working 搜索组件

--- src/base/search-bar/search-bar.vue
+++ src/base/search-bar/search-bar.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="search-wrapper">
     <div class="search-box">
-      <input v-model="keyword" class="search-input" type="text" :placeholder="placeholder" @focus="keyword=''" @blur="keyword='实力派'">
+      <input v-model="keyword" class="search-input" type="text" :placeholder="placeholder" @focus="_focusHandler"
+             @blur="_blurHandler">
       <input class="search-btn" type="button" value="搜索">
     </div>
-    <div class="pos-guess-wrapper">
+    <div class="pos-guess-wrapper" ref="guess">
       <div class="guess-wrapper">
         <span>猜你要搜</span>
         <ul>
-          <li v-for="i in guessItems"></li>
+          <li v-for="i in guessItems" @click="chooseItem(i)">{{i}}</li>
         </ul>
       </div>
     </div>
@@ -33,6 +34,22 @@
           'h5',
           '实习'
         ]
+      }
+    },
+    methods: {
+      _focusHandler() {
+        this.keyword = ''
+        this.$refs.guess.style.display = 'block'
+      },
+      _blurHandler() {
+        this.keyword = '实力派'
+        setTimeout(() => {
+          this.$refs.guess.style.display = 'none'
+        }, 20)
+      },
+      chooseItem(item) {
+        console.log('666666')
+        this.keyword = item
       }
     }
   }
@@ -72,7 +89,11 @@
         color #fff
         border 0
         background #00b38a
+        outline 0 !important
+        &:hover
+          background #00a57f
     .pos-guess-wrapper
+      display none
       position relative
       width 938px
       height 0
@@ -80,13 +101,24 @@
       .guess-wrapper
         position absolute
         z-index 150
-        width 795px
+        width 762px
         background #fff
-        box-shadow 0 2px 3px 0 rgba(0,0,0,.08)
+        box-shadow 0 2px 3px 0 rgba(0, 0, 0, .08)
         border-radius 3px
         font-size 14px
         span
           display block
           color #999
           padding 5px 17px
+        ul
+          margin 0
+          padding 0
+          list-style none
+          li
+            color #555
+            padding 8px 17px
+            cursor pointer
+            &:hover
+              background-color #00b38a
+              color #fff
 </style>
