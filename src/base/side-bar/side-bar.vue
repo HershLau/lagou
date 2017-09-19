539842d612b2d4b26c35c97483a8682f11b8fd9e
hersh <neu_lhx@163.com>
2017-09-19 18:49:24 +0800

    working 侧边栏

---
+++ src/base/side-bar/side-bar.vue
@@ -0,0 +1,189 @@
+<template>
+  <div id="main-navs">
+    <div class="menu-box" :class="{current: isHover===category.name}" @mouseover="hoverHandler(category.name)" v-for="category in categoryList">
+      <div class="menu-main" >
+        <div class="category-list">
+          <h2>{{category.name}}</h2>
+          <a v-for="h in category.hotWords">{{h}}</a>
+          <i class="arrow"></i>
+        </div>
+      </div>
+      <div class="menu-sub" v-show="isHover===category.name">
+        <dl v-for="(arr, key) in category.detail">
+          <dt>
+            <span>{{key}}</span>
+          </dt>
+          <dd>
+            <a v-for="i in arr">{{i}}</a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        categoryList: [
+          {
+            name: '技术',
+            hotWords: ['Java', 'PHP', 'C++', 'Android'],
+            detail: {
+              '后端开发': ['Java', 'C++', '数据挖掘', '搜索算法', '精准推荐', 'C', 'C#', '全栈工程师', '.NET', 'Hadoop', 'Python', 'Delphi', 'VB', 'Perl', 'Ruby', 'Node.js', 'Go', 'ASP', 'Shell', '后端开发其他'],
+              '移动开发': ['HTML5', 'Android', 'ios', 'WP', '移动开发其他']
+            }
+          },
+          {
+            name: '产品',
+            hotWords: ['产品总监', '产品经理'],
+            detail: {
+              '产品经理': ['产品经理', '网页产品经理', '移动产品经理', '产品助理', '数据产品经理', '电商产品经理', '游戏策划', '产品实习生']
+            }
+          },
+          {
+            name: '设计',
+            hotWords: ['UI设计师', '交互设计', '网页设计师'],
+            detail: {
+              '视觉设计': ['视觉设计师', '网页设计师', 'Flash设计师']
+            }
+          },
+          {
+            name: '运营',
+            hotWords: ['新媒体运营', '编辑', '数据运营'],
+            detail: {}
+          },
+          {
+            name: '市场与销售',
+            hotWords: ['市场推广', '市场总监'],
+            detail: {}
+          },
+          {
+            name: '职能',
+            hotWords: ['HR', '行政', '财务', '审计'],
+            detail: {}
+          },
+          {
+            name: '金融',
+            hotWords: ['投资', '融资', '并购', '风控'],
+            detail: {}
+          }
+        ],
+        isHover: false
+      }
+    },
+    methods: {
+      hoverHandler(name) {
+        this.isHover = name
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/base"
+
+  #main-navs
+    .current
+      .menu-main
+        box-shadow 0 0 4px 2px rgba(0, 0, 0, .09)
+        border-top-left-radius 2px
+        border-bottom-left-radius 2px
+        background #fff
+        padding 18px 0 17px
+        position relative
+        outline 0
+      &:after
+        content ''
+        position absolute
+        top 0
+        bottom 0
+        right -1px
+        width 7px
+        background #fff
+        z-index 102
+    a
+      color #555
+      background-color transparent
+      text-decoration none
+    .menu-box
+      position relative
+      .menu-main
+        position relative
+        padding 18px 0 17px
+        .category-list
+          position relative
+          height 20px
+          line-height 20px
+          overflow hidden
+          border-right 0
+          h2
+            display inline-block
+            padding-left 15px
+            margin 0
+            font-size 18px
+            font-weight 500
+          a
+            margin 0 5px 5px 0
+            padding 0 8px
+            white-space nowrap
+            cursor pointer
+            &:hover
+              color #00b38a
+              text-decoration underline
+          .arrow
+            position absolute
+            right 0
+            top 6px
+            width 6px
+            height 11px
+            margin-right 8px
+            background url(//static.lagou.com/www/static/index/modules/sidebar/img/arrow_32aaad2.png) 0 0 no-repeat
+      .menu-sub
+        position absolute
+        left 321px
+        top 0
+        width 600px
+        padding 20px 10px 0 12px
+        z-index 10
+        background #fff
+        box-shadow 0 0 4px 2px rgba(0, 0, 0, .09)
+        border-radius 2px
+        border-top-left-radius 0
+        dl
+          margin 0 0 17px
+          dt
+            position relative
+            width 70px
+            margin 0
+            padding-left 10px
+            font-size 14px
+            line-height 20px
+            margin-bottom 6px
+            span
+              position relative
+              display inline-block
+              padding 0
+              color #333
+          dd
+            color #555
+            font-size 0
+            overflow hidden
+            a
+              display inline-block
+              position relative
+              margin-left -1px
+              padding 0 11px
+              font-size 14px
+              line-height 28px
+              white-space nowrap
+              &:before
+                content ''
+                position absolute
+                width 1px
+                height 12px
+                background #d8d8d8
+                top 8px
+                left 0
+</style>
--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,19 +1,37 @@
 <template>
-  <div>
+  <div id="index">
     <search-bar></search-bar>
+    <div class="container-body">
+      <div class="clearfix">
+        <div class="side-bar">
+          <side-bar></side-bar>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import SearchBar from 'base/search-bar/search-bar'
+  import SideBar from 'base/side-bar/side-bar'
 
   export default {
     components: {
-      SearchBar
+      SearchBar,
+      SideBar
     }
   }
 
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  #index
+    .container-body
+      margin 26px auto 0
+      width 1200px
+      .side-bar
+        float left
+        position relative
+        width 321px
+        margin-left -15px
 </style>
